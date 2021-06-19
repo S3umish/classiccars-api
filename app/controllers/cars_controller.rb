6704dbcab84c276c55747215ec8a1f106dbee6a5
sunitee <suniteebajpai2@gmail.com>
2021-06-19 18:08:28 -0400

    added controller action basic setup

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,8 +1,53 @@
 class CarsController < ApplicationController
 
    def index
-    cars = Car.all
-    render json: cars.to_json(except: [:created_at, :updated_at])
+    @cars = Car.all
+    render json: @cars.to_json(except: [:created_at, :updated_at])
    end
-   
+
+   # GET /cars/1
+  def show
+    render json: @car
+  end
+
+  # POST /cars
+  def create
+    @car = Car.new(car_params)
+
+    if @car.save
+      render json: @car, status: :created, location: @car
+    else
+      render json: @car.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /cars/1
+  def update
+    if @car.update(car_params)
+      render json: @car
+    else
+      render json: @car.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /cars/1
+  def destroy
+    @car.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_car
+      @car = Car.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def car_params
+      params.require(:car).permit(:name, :description, :image_url, :rating, :available)
+    end
+
+
+
+
+
 end
